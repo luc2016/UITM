b30b1cbaeb7072b065490d060081b482c360d3b1
Lu Cui <lu.cui@loblaw.ca>
2018-04-15 21:37:57 -0400

    try

---
+++ ui_report/lib/UITM.swift
@@ -0,0 +1,17 @@
+//
+//  MyFramework.swift
+//  ui_report
+//
+//  Created by Lu Cui (LCL) on 2018-04-15.
+//  Copyright © 2018 lcl. All rights reserved.
+//
+
+import Foundation
+
+public class UITM{
+    static let shared = UITM()
+    
+    func foo(){
+        
+    }
+}
