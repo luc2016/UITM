0a9d3c9076a71cecf810d97cd51f989977b9d86f
Lu Cui <lu.cui@loblaw.ca>
2018-04-23 22:17:14 -0400

    try add unit tests

--- UITMTests/UITMTests.swift
+++ UITMTests/UITMTests.swift
@@ -14,6 +14,7 @@ class UITMTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
@@ -22,16 +23,10 @@ class UITMTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testMetaData() {
+        self.metaData = "GOLM-T1"
+        XCTAssert(self.metaData == "GOLM-T1")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
     
 }
